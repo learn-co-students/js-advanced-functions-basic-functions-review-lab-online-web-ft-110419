b5b4cbce47e6c1e0a87dd83312a9096864fdc426
jessesbyers <jesse.s.byers@gmail.com>
2020-02-28 09:18:50 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ function wrapAdjective(string = "*") {
         return `You are ${string}${adjective}${string}!`
     }
 }
+
+const Calculator = {
+    add: function() {return 1 + 3}, 
+    subtract: function() {return 1 - 3}, 
+    multiply: function() {return 1 * 3}, 
+    divide: function() {return 10 / 5}
+}
+
+function actionApplyer(integer, array) {
+    if (array.length === 0) {
+        return integer 
+    } 
+    if (array.length !== 0) {
+        const [calc1, calc2, calc3] = array
+        let answer1 = calc1(integer);
+        let answer2 = calc2(answer1);
+        return calc3(answer2)
+    }
+}
